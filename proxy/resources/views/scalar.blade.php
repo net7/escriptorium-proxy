73b22d9a753bba9462d06d51e43a68805ea47e62
Nicola <baglini@netseven.it>
2026-01-24 17:57:32 +0100

    refactor: Use dynamic HTML language and page title, and remove inline body styles.

--- proxy/resources/views/scalar.blade.php
+++ proxy/resources/views/scalar.blade.php
@@ -1,15 +1,10 @@
 <!doctype html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <title>eScriptorium Proxy API - Scalar</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <style>
-        body {
-            margin: 0;
-        }
-    </style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 </head>
 
 <body>
